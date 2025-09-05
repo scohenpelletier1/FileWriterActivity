545ac77b11aa42c03d7019a665fd386daab18d44
Sophia <scohenpelletier1@hwemail.com>
2025-09-05 02:00:05 -0700

    Created secret stuff & .gitignore
    Add hidden and non-hidden files using chosen writing method; create .gitignore file

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -3,7 +3,34 @@ import java.nio.file.*;
 import java.nio.charset.StandardCharsets;
 
 public class MyFileWriter {
-    public static void main(String[] args) {
+    // secret password file creator
+    public static void secretPassword() throws IOException {
+        // make secret password
+        BufferedWriter secretPassword = new BufferedWriter(new FileWriter(".whoNeeds2FA.txt"));
+        secretPassword.write("My genius, impossible to break password: password123");
+
+        // close bufferedWriter
+        secretPassword.close();
+
+    }
+
+    public static void confidentialPlans() throws IOException {
+        // get file paths
+        String directory = "/Users/scp/Desktop/School/12th Grade/Honors Topics/HTCS_Projects/FileWriterActivity/.topSecret";
+
+        // put the secret file in the .topSecret file
+        File secretFile = new File(directory, "plansForWorldDomination.dat");
+
+        // make secret plans
+        BufferedWriter secretPlans = new BufferedWriter(new FileWriter(secretFile));
+        secretPlans.write("Step 1: Eat breakfast \nStep 2: Discover the meaning of life \nStep 3: ??? \nStep 4: Profit");
+
+        // close bufferedWriter
+        secretPlans.close();
+
+    }
+
+    public static void main(String[] args) throws IOException {
         String data = "Hello, World!";
         String fileName1 = "example.txt";
         String fileName2 = "example2.txt";
@@ -56,6 +83,10 @@ public class MyFileWriter {
 
         }
 
-    }
+        // SEE IF THE SECRET PLAN STUFF WORKS
+        secretPassword();
+        confidentialPlans();
 
+    }
+    
 }
